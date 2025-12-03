9285a62ecb52274227ec35324a2cc7e0ae4a037d
Kylath George <kyla0001@algonquinlive.com>
2025-12-03 12:51:10 -0500

    Changed store admin logo and nav background colour

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/bestlogo.png" alt="Best Buy Store Logo">
       </a>
       Admin Portal
     </div>
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #333;
+  background-color: #0748d4d7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
